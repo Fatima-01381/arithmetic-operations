be5d4873c52214c01067bf5af67d7fc0c6ee9355
Fatima-01381 <fatimagajimi0@gmail.com>
2024-10-03 22:30:22 +0100

    Add files via upload

---
+++ arithmetic operation.py
@@ -0,0 +1,18 @@
+Enter the first number: 1
+Enter the second number: 3
+
+Results:
+Addition: 1.0 + 3.0 = 4.0
+Subtraction: 1.0 - 3.0 = -2.0
+Multiplication: 1.0 * 3.0 = 3.0
+Division: 1.0 / 3.0 = 0.3333333333333333
+Enter the first number:
+Traceback (most recent call last):
+  File "/data/user/0/ru.iiec.pydroid3/files/accomp_files/iiec_run/iiec_run.py", line 31, in <module>
+    start(fakepyfile,mainpyfile)
+  File "/data/user/0/ru.iiec.pydroid3/files/accomp_files/iiec_run/iiec_run.py", line 30, in start
+    exec(open(mainpyfile).read(),  __main__.__dict__)
+  File "<string>", line 26, in <module>
+ValueError: could not convert string to float: ''
+
+[Program finished]
